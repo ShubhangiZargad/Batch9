3efba3f701c02be53c6da22715cf876308efcfa2
ShubhangiZargad <shubhangizargad97@gmail.com>
2023-02-24 21:51:01 +0530

    all_operatin

--- All_operaion.py
+++ All_operaion.py
@@ -4,7 +4,7 @@ c = a + b
 d = a- b
 e = a * b
 f = a / b
-print(f" addition is {c}")
-print(f" subtraction is {d}")
-print(f" multiplication is {e}")
-print(f" division is {f}")
+print(f" addition of  {a} and {b} is {c}")
+print(f" subtraction of  {a} and {b} is {d}")
+print(f" multiplication of {a} and {b} is {e}")
+print(f" division  of {a} and {b} is {f}")
